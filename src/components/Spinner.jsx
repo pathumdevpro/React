dab8af22b2a248777f272e7f422e660216e12faf
Pathum Bandara <pathumbandarame@gmail.com>
2024-09-14 01:04:09 +0530

    add dynamic jobs

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,21 @@
+import { ClipLoader } from "react-spinners"
+
+const override = {
+    display: 'block',
+    margin: '100px auto'
+}
+
+// eslint-disable-next-line react/prop-types
+const Spinner = ({ loading }) => {
+
+  return (
+    <ClipLoader 
+        color='#4338ca'
+        loading={loading}
+        cssOverride={override}
+        size={150}
+    />
+  );
+};
+
+export default Spinner
